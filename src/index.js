45003c17a4e1553f6bb756ddb5cf10d21d324553
ViktorK66 <55109336+ViktorK66@users.noreply.github.com>
2020-03-22 07:26:53 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length==0) return 0;
+  let min=array[0];
+  for (let i=1;i<array.length;i++){
+    min=min>array[i]?array[i]:min;
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length==0) return 0;
+  let max=array[0];
+  for (let i=1;i<array.length;i++){
+    max=max<array[i]?array[i]:max;
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length==0) return 0;
+  let avg=0;
+  for(let i=0;i<array.length;i++){
+    avg+=array[i];
+  }
+  return avg/array.length;
 }
